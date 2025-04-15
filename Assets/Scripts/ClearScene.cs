b70ab4da1b5079326ba7f5cdbc6850d3a3ba6491
it242007 <it242007@GAME242007>
2025-04-15 19:27:54 +0900

    クリアシーンからタイトルへ

--- Assets/Scripts/ClearScene.cs
+++ Assets/Scripts/ClearScene.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
-public class BackGround : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class ClearScene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,7 +13,9 @@ public class BackGround : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
     }
 }
