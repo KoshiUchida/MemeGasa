ac6f8ca99b31840590b0490a98f0517d743e775b
it242007 <it242007@GAME242007>
2025-04-18 18:50:37 +0900

    チュートリアル途中

--- Assets/Ui.cs
+++ Assets/Ui.cs
@@ -9,23 +9,47 @@ public class Ui : MonoBehaviour
     public GameObject textBox;
     public TMP_Text tutorialText;
 
+    public GameObject Camera;
+
+    public int X;
     private void Start()
     {
         StartCoroutine(ShowTutorial());
+
+
+    }
+
+    private void Update()
+    {
+        //X = Camera.GetComponent<CameraManager>().
+
+
+
     }
 
 
     private IEnumerator ShowTutorial()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0f);
 
         tutorialText.text = "左へ移動←右へ移動→";
+        Debug.Log("左右");
         textBox.SetActive(true);
 
-        yield return new WaitForSeconds(200f);
+        yield return new WaitForSeconds(2f);
 
         textBox.SetActive(false);
-    }
+        Debug.Log("消えた");
 
+
+        tutorialText.text = "↑キーで箱移動";
+        Debug.Log("hako");
+        textBox.SetActive(true);
+
+        yield return new WaitForSeconds(7f);
+
+        textBox.SetActive(false);
+
+    }
    
 }
